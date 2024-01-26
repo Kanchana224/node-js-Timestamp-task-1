d15383709289cecf4d4202bfd8e9bca2ffc73031
Kanchana224 <kkanchana427@gmail.com>
2024-01-26 17:01:22 +0530

    updated

---
+++ index.js
@@ -0,0 +1,24 @@
+const PORT = 8000;
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  try {
+    let today = new Date();
+    let formattedDate = `${today.toLocaleDateString()} ${today.toLocaleTimeString()}`;
+    let fileName = `TimeStamp/${today.getTime()}.txt`;
+    fs.writeFileSync(fileName, formattedDate, "utf8");
+    let data = fs.readFileSync(fileName, "utf8");
+    res.writeHead(200, {
+      "content-type": "text/html"
+    });
+    res.end(`<h1>Current Day and Time:</h1><p>${data}</p>`);
+    } catch (error) {
+    console.log(error);
+    res.writeHead(500, {
+      "content-type": "text/html"
+    });
+    res.end("<h1>Internal Server Error</h1>");
+  }
+});
+
+server.listen(PORT, () => console.log(`app listening to ${PORT}`));
